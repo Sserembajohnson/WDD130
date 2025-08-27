78019532f4adf6b618707e196e7be658bd22cb57
Johnsseremba <ssrmbjohnson@gmail.com>
2025-08-27 18:28:17 +0300

    123wdd

---
+++ Mad libs.py
@@ -0,0 +1,16 @@
+#Mad_libs.py
+adjective=input("fluffy:")
+animal=input("llama:")
+verb1=input("dancing:")
+exclamation=input("yikes:")
+verb2=input("scream:")
+verb3=input("moonwalk:")
+
+print("\nHere's my mad lib story! |n")
+print("the other day, I was really in trouble." \
+"it all started when I saw avery")
+print(f"{fluffy} {llama} {dancing:} down the
+   hallway.\ "{yikes}!|" I yelled. But all")
+print(f 'I could think to do was to {scream} '
+'over and over. miraculously,')
+      
